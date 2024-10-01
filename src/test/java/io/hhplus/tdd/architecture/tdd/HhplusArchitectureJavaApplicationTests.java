e5991d5918ee952db65af2ad4836f24c1d447287
Linverno <fatal_linverno@naver.com>
2024-10-01 23:13:57 +0900

    First Commit

---
+++ src/test/java/io/hhplus/tdd/architecture/tdd/HhplusArchitectureJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hhplus.tdd.architecture.tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusArchitectureJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
