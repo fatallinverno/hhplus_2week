0bbc0c6322b35496e5e4b91ef0179cb15e8b5ebe
Linverno <fatal_linverno@naver.com>
2024-10-01 23:17:18 +0900

    강의 Controller 추가

---
+++ src/main/java/io/hhplus/tdd/architecture/tdd/Presentation/Controller/LectureController.java
@@ -0,0 +1,35 @@
+package io.hhplus.tdd.architecture.tdd.Presentation.Controller;
+
+import io.hhplus.tdd.Application.Service.LectureService;
+import io.hhplus.tdd.Domain.Entity.Lecture;
+import io.hhplus.tdd.Domain.Entity.LectureHistory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(("/lecture"))
+public class LectureController {
+
+    private final LectureService lectureService;
+
+    public LectureController(LectureService lectureService) {
+        this.lectureService = lectureService;
+    }
+
+    public List<Lecture> getLectures() {
+        return lectureService.getLectureAll();
+    }
+
+    public Lecture createLecture(String createTitle) {
+        return lectureService.createLecture(createTitle);
+    }
+
+    @GetMapping("{id}/join")
+    public LectureHistory joinLecture(long userId, Long id) {
+        return lectureService.joinLecture(userId, id);
+    }
+
+}
