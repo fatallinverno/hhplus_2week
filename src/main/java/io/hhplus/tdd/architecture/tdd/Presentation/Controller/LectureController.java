912319d8c68e664fe64f8564c577e99c82a3c131
Linverno <fatal_linverno@naver.com>
2024-10-01 23:39:58 +0900

    Lecture Controller import 경로 수정

--- src/main/java/io/hhplus/tdd/architecture/tdd/Presentation/Controller/LectureController.java
+++ src/main/java/io/hhplus/tdd/architecture/tdd/Presentation/Controller/LectureController.java
@@ -1,8 +1,8 @@
 package io.hhplus.tdd.architecture.tdd.Presentation.Controller;
 
-import io.hhplus.tdd.Application.Service.LectureService;
-import io.hhplus.tdd.Domain.Entity.Lecture;
-import io.hhplus.tdd.Domain.Entity.LectureHistory;
+import io.hhplus.tdd.architecture.tdd.Application.Service.LectureService;
+import io.hhplus.tdd.architecture.tdd.Domain.Entity.Lecture;
+import io.hhplus.tdd.architecture.tdd.Domain.Entity.LectureHistory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
