707e81cc158bdd71c759ec622979f7c429675451
Linverno <fatal_linverno@naver.com>
2024-10-03 03:37:19 +0900

    신청 내역 조회 쿼리 추가

--- src/main/java/io/hhplus/tdd/architecture/tdd/infrastructure/repository/history/LectureHistoryRepository.java
+++ src/main/java/io/hhplus/tdd/architecture/tdd/infrastructure/repository/history/LectureHistoryRepository.java
@@ -1,10 +1,16 @@
 package io.hhplus.tdd.architecture.tdd.infrastructure.repository.history;
 
 
+import io.hhplus.tdd.architecture.tdd.domain.entity.Lecture;
 import io.hhplus.tdd.architecture.tdd.domain.entity.LectureHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface LectureHistoryRepository extends JpaRepository<LectureHistory, Long> {
+    @Query("SELECT l FROM LectureHistory l WHERE l.userId = :userId")
+    Optional<Lecture> findByLectureUserId(String userId);
 }
