8082adf3a34b1855c32344eb5b77e60854cd8798
Linverno <fatal_linverno@naver.com>
2024-10-03 23:38:33 +0900

    DB에 등록된 유저 정보와 강의 정보를 가지고온 후 통합테스트 코드 수정

--- src/test/java/io/hhplus/tdd/architecture/tdd/LectureApplyTest.java
+++ src/test/java/io/hhplus/tdd/architecture/tdd/LectureApplyTest.java
@@ -2,11 +2,15 @@ package io.hhplus.tdd.architecture.tdd;
 
 import io.hhplus.tdd.architecture.tdd.application.service.LectureService;
 import io.hhplus.tdd.architecture.tdd.domain.entity.Lecture;
+import io.hhplus.tdd.architecture.tdd.domain.entity.User;
+import io.hhplus.tdd.architecture.tdd.infrastructure.repository.lecture.LectureRepository;
+import io.hhplus.tdd.architecture.tdd.infrastructure.repository.user.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -21,21 +25,29 @@ public class LectureApplyTest {
 
     private Lecture lecture;
 
-    @BeforeEach
-    void setUp() {
-        lecture = new Lecture();
-    }
+    @Autowired
+    private LectureRepository lectureRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     @Test
     public void testLectureApply() throws InterruptedException {
+        //Given
+        lecture = lectureRepository.findById(1L).orElseThrow(() -> new IllegalArgumentException("강의가 없습니다."));
+
         List<Thread> threads = new ArrayList<>();
         List<Boolean> results = Collections.synchronizedList(new ArrayList<>());
 
-        for (int i = 1; i <= 40; i++) {
-            final String userId = "user" + i;
+        List<User> users = userRepository.findAll();
+
+        assertEquals(40, users.size());
+
+        for (int i = 0; i < 40; i++) {
+            final User user = users.get(i);
+
             Thread thread = new Thread(() -> {
                 try {
-                    lectureService.joinLecture(userId, 1L);
+                    lectureService.joinLecture(user.getUserId(), lecture.getLectureId());
                     results.add(true);
                 } catch (Exception e) {
                     results.add(false);
@@ -52,9 +64,14 @@ public class LectureApplyTest {
             thread.join();
         }
 
-        long successCount = lecture.getCapacity();
+        lecture = lectureRepository.findById(1L).orElseThrow(() -> new IllegalArgumentException("강의가 없습니다."));
 
-        assertEquals(0, successCount);
+        long successCount = results.stream().filter(result -> result).count();
+        long failCount = results.stream().filter(result -> !result).count();
+
+        assertEquals(0, lecture.getCapacity());
+        assertEquals(30, successCount);
+        assertEquals(10, failCount);
     }
 
 }
