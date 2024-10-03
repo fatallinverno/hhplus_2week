61b7786dc8200b19857b84c5e6d46775713deb61
Linverno <fatal_linverno@naver.com>
2024-10-03 23:42:30 +0900

    Entity 쿼리 수정

--- src/main/java/io/hhplus/tdd/architecture/tdd/infrastructure/repository/lecture/LectureRepository.java
+++ src/main/java/io/hhplus/tdd/architecture/tdd/infrastructure/repository/lecture/LectureRepository.java
@@ -6,16 +6,18 @@ import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface LectureRepository extends JpaRepository<Lecture, Long> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Query("SELECT l.capacity FROM Lecture l WHERE l.lectureId = :lectureId")
-    int findByCapacityLock(Long lectureId);
+    @Query("SELECT l FROM Lecture l WHERE l.lectureId = :lectureId")
+    Optional<Lecture> findByIdWithLock(@Param("lectureId") Long lectureId);
 
     List<Lecture> findAllByLectureDate(LocalDate lectureDate);
 }
