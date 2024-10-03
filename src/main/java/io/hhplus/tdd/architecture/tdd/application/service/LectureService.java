4591dcd775da91b5d472b22ee397bb2c95a7c65a
Linverno <fatal_linverno@naver.com>
2024-10-03 20:17:34 +0900

    공백 제거

--- src/main/java/io/hhplus/tdd/architecture/tdd/application/service/LectureService.java
+++ src/main/java/io/hhplus/tdd/architecture/tdd/application/service/LectureService.java
@@ -7,9 +7,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface LectureService {
-
     List<Lecture> getLectureAll(LocalDate lectureDate);
     Lecture createLecture(String createTitle);
     LectureHistory joinLecture(String userId, Long lectureId);
-
 }
